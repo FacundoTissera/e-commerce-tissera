c2c37c1179fd1bd27c24d046175f4ca9680a687d
Facundo <facundotisserasorribas@gmail.com>
2022-04-23 11:23:08 -0300

    creacion de contador de productos y estilos

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -5,12 +5,14 @@ import "../assets/sass/itemCount.scss";
 
 
 
-function ItemCount({ stock, inicial }) {
-
+function ItemCount({ stock, inicial, productos }) {
     const [count, setCount] =  useState(inicial);
-
+    
+   
+    
     const [agregar, setAgregar] = useState(false);
 
+  
     const contadorAdd =  () => {
         setCount(count + 1);
         
@@ -33,19 +35,31 @@ function ItemCount({ stock, inicial }) {
     return (
         <>
         <div className='container-btns'>
-            <div className='container-min-plus'>
-                
-                <button className='btn-count' onClick={contadorMinus}>➖ </button>
-                    <span>{count}</span>
-                <button className='btn-count' onClick={contadorAdd}>➕ </button>
-            </div>
+            {
+                productos.map((producto, i) => {
+                    return (
+                        <div className='container-productos' key={i}>
+                            <div >
+                                <p>{producto.nombre}</p>
+                                <p>${producto.precio}</p>
+                                <p>{producto.categoria}</p>
+                            </div>    
+                            <div className='container-min-plus'>
 
-                <button className='btn-add' onClick={addProduct}>AGREGAR AL CARRITO</button>
-            
+                                <button className='btn-count' onClick={contadorMinus}>➖ </button>
+                                    <span>{count}</span>
+                                <button className='btn-count' onClick={contadorAdd}>➕ </button>
+                            </div>
+                        
+                            <button className='btn-add' onClick={addProduct}>AGREGAR AL CARRITO</button>
+
+                        </div>
+                    )
+                })
+            }
+                {agregar && <p>Producto agregado al carrito, la cantidad agregada es {count}</p>}
+                {stock < count && <p>No hay suficiente stock para la cantidad solicitada</p>}
         </div>
-
-        {agregar && <p>Producto agregado al carrito, la cantidad agregada es {count}</p>}
-        {stock < count && <p>No hay suficiente stock para la cantidad solicitada</p>}
         </>
     )
 }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,5 +1,30 @@
 import ItemCount from "./ItemCount";
-
+let productos = [
+  {
+      "id": "1",
+      "nombre": "Coca Cola",
+      "precio": "2.00",
+      "stock": "8",
+      "categoria": "Bebidas"
+      
+  },
+  {
+      "id": "2",
+      "nombre": "Fanta",
+      "precio": "2.00",
+      "stock": "11",
+      "categoria": "Bebidas"
+      
+  },
+  {
+      "id": "3",
+      "nombre": "Sprite",
+      "precio": "2.00",
+      "stock": "10",
+      "categoria": "Bebidas"
+      
+  }
+]
 function ItemListContainer({ producto  }) {
 
     // creacion de contador
@@ -7,7 +32,7 @@ function ItemListContainer({ producto  }) {
   return (
     <div>
       {producto}
-      <ItemCount stock={10} inicial={1} />    
+      <ItemCount productos={productos} stock={10} inicial={1} />    
     </div>
 
   )
