e15610f98f7159278f114d2f3aef24a972bb5e01
Facundo <facundotisserasorribas@gmail.com>
2022-04-28 18:01:17 -0300

    modificacion en itrem count para que renderice botones de agregado al carrito

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,25 +1,9 @@
 import ItemCount from "./ItemCount";
-import { useState } from "react";
-import swAlert from "@sweetalert/with-react";
 
 
 function Item({data}) {
 
-  
-     // estado de boton agregado o no 
-    const [agregar, setAgregar] = useState(false);
-
-    
-  const addProduct = () => {
-    
-    setAgregar(true);
-    swAlert({
-        title: "Producto Agregado",
-        text: "El producto se ha agregado al carrito",
-        icon: "success",
-        button: "Aceptar",
-    });
-}
+ 
     
   return (
     <div className='container-productos' key={data.id}>
@@ -30,8 +14,8 @@ function Item({data}) {
             <p>El stock de este Producto es de {data.stock}</p>
 
             <ItemCount />
-            <button className='btn-add' onClick={addProduct}>AGREGAR AL CARRITO</button>
-            <button className='btn-add' ><a href="/item"> IR AL DETALLE </a></button>
+            {/* <button className='btn-add' onClick={addProduct}>AGREGAR AL CARRITO</button>
+            <button className='btn-add' ><a href="/item"> IR AL DETALLE </a></button> */}
           </div>    
                         
                     
--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import swAlert from "@sweetalert/with-react";
 
 
 function ItemCount() {
@@ -8,40 +9,53 @@ function ItemCount() {
     const [count, setCount] =  useState(0);
     
 
-
-
+ 
+     // estado de boton agregado o no 
+     const [agregar, setAgregar] = useState(false);
+     
      // contador mas e implementacion de swalert para agregar productos al carrito
-    const contadorAdd =  () => {
-        setCount(count + 1);
-    }
-
-
-
-     // contador menos e implementacion eliminar contador productos del carrito
-     const contadorMinus =  () => {
-        
-        if (count < 2) {
-            setCount(0); 
-        }else{
-            setCount(count - 1);
+     const contadorAdd =  () => {
+         setCount(count + 1);
         }
         
-    };
-
-
-    // boton de agregar al carrito con alerta para agragado
- 
-  return (
-    <div>
+        
+        
+        // contador menos e implementacion eliminar contador productos del carrito
+        const contadorMinus =  () => {
+            
+            if (count < 2) {
+                setCount(0); 
+            }else{
+                setCount(count - 1);
+            }
+            
+        };
+        
+        
+        // boton de agregar al carrito con alerta para agragado
+        
+        const addProduct = () => {
+            
+            setAgregar(true);
+            swAlert({
+                title: "Producto Agregado",
+                text: "El producto se ha agregado al carrito",
+                icon: "success",
+                button: "Aceptar",
+            });
+       }
+        
+       return (
+           <div>
         <div  className='container-min-plus'>
             <button className='btn-count' onClick={contadorMinus}>➖ </button>
             <span>{count}</span>
             <button className='btn-count' onClick={contadorAdd}>➕ </button>
         </div>
 
-        {/* <button className='btn-add' onClick={addProduct}>AGREGAR AL CARRITO</button>
+        <button className='btn-add' onClick={addProduct}>AGREGAR AL CARRITO</button>
                 
-        <button className='btn-add' ><a href="/item"> IR AL DETALLE </a></button> */}
+        <button className='btn-add' ><a href="/item"> IR AL DETALLE </a></button>
     </div>
   )
 }
